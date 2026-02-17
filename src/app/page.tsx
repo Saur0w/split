6064422a9bb45a16587b90842ecefafe6927ccd4
saur0w <sthapliyal085@gmail.com>
2026-02-17 18:50:00 +0530

    "Gallery"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import styles from "./page.module.css";
 import Gallery from "@/components/gallery/index";
 import Lenis from 'lenis';
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 interface Project {
     name: string;
@@ -31,6 +31,8 @@ const projects: Project[] = [
 
 export default function Home() {
 
+    const [mousePosition, setMousePosition] = useState({x: 0, y: 0});
+
     useEffect(() => {
         const lenis = new Lenis();
 
@@ -46,8 +48,12 @@ export default function Home() {
         };
     }, []);
 
+    const mouseMove = (e: React.MouseEvent) => {
+
+    }
+
     return (
-        <main className={styles.main}>
+        <main onMouseMove={mouseMove} className={styles.main}>
             {
                 projects.map(({handle}, i) => {
                     return <Gallery handle={handle} key={i}/>
--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -2,12 +2,37 @@
 
 import styles from './style.module.scss';
 import Image from 'next/image';
+import { useRef, RefObject} from 'react';
+import gsap from 'gsap';
+import { useGSAP } from "@gsap/react";
 
 interface GalleryProps {
+    mousePosition: {
+        x: RefObject<number>;
+        y: RefObject<number>;
+    };
     handle: string;
 }
 
-export default function Gallery({ handle}: GalleryProps) {
+export default function Gallery({ mousePosition, handle}: GalleryProps) {
+    const vignetteRef = useRef<HTMLDivElement>(null);
+
+    useGSAP(() => {
+        const updatePosition = () => {
+            if (!vignetteRef.current) return;
+
+            const currentX = mousePosition.x.current;
+            const currentY = mousePosition.y.current;
+
+            gsap.set(vignetteRef.current, {x: currentX, y: currentY});
+        };
+
+        gsap.ticker.add(updatePosition);
+
+        return () => {
+            gsap.ticker.remove(updatePosition);
+        };
+    }, []);
     return (
         <section className={styles.gallery}>
             <div className={styles.imageContainer}>
@@ -17,11 +42,11 @@ export default function Gallery({ handle}: GalleryProps) {
                     fill
                 />
             </div>
-            <div className={styles.vignetter}>
+            <div className={styles.vignetter} ref={vignetteRef}>
                 <Image
                     src={`/images/${handle}/1.jpg`}
-                    alt="vignetter"
-                    layout="fill"
+                    alt="vignette image"
+                    fill
                     />
             </div>
         </section>
